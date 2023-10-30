251399506280539ac65de555b27dd20370dc09e8
tamnguyen02121993 <tamnguyen02121993@gmail.com>
2023-10-30 22:26:21 +0700

    Update extension file

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.scss'
 
-// const root = document.createElement('div')
-// root.id = 'crx-root'
-// document.body.append(root)
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
